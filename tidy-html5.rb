eeb7a24718a74980aa58a011356601986c8dae74
Maverick JS <mavjs01@gmail.com>
2014-10-17 14:10:05 +0200

    oops! accidental removal of version

--- tidy-html5.rb
+++ tidy-html5.rb
@@ -4,6 +4,7 @@ class TidyHtml5 < Formula
   homepage 'http://w3c.github.com/tidy-html5/'
   url 'https://github.com/w3c/tidy-html5/archive/master.zip'
   sha1 '45634577b7605ffa547778eddb42b81d87da519b'
+  version "20130103"  
 
   def install
     ENV.deparallelize
