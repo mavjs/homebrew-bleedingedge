642082b8d8d5d73233be47cf79a1a2bf308650e7
Maverick JS <mavjs01@gmail.com>
2014-10-17 14:13:26 +0200

    build tidy-html5 from head instead

--- tidy-html5.rb
+++ tidy-html5.rb
@@ -2,9 +2,7 @@ require 'formula'
 
 class TidyHtml5 < Formula
   homepage 'http://w3c.github.com/tidy-html5/'
-  url 'https://github.com/w3c/tidy-html5/archive/master.zip'
-  sha1 '45634577b7605ffa547778eddb42b81d87da519b'
-  version "20130103"  
+  head 'https://github.com/w3c/tidy-html5.git'
 
   def install
     ENV.deparallelize
